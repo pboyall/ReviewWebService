effba1c907fe8e87083d3ae2ea67be31fa7bb31a
pboyall <peter.boyall@popewoodhead.com>
2015-10-24 17:38:51 +0100

    move to macbook

--- src/Tests/TestWorkflowFunctions.js
+++ src/Tests/TestWorkflowFunctions.js
@@ -4,6 +4,5 @@ var w = require('../Workflow.js');
 console.log(wff.getStartNode(3));
 console.log(wff.Initialise(1, 3));
 
-w.(1, 3);
-
-w.InitialiseWorkflow(1, 3);
+w.InitialiseWorkflow(1, 3)
+    .then();
